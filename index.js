cb62c64a887e120e58dad997ef744ba0a67b1427
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-12-07 09:32:17 +0530

    Add Prepend and refactor append function

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const newLinkedList = (name) => {
   const getHead = () => {
     return head;
   };
-  const append = (node) => {
+  const append = (value) => {
     //   console.log("is this working");
 
     if (head === null) {
       // console.log("inside 1");
-      head = node;
+      head = newNode(value);
       return;
     } else {
       // console.log("inside 2");
@@ -23,20 +23,27 @@ const newLinkedList = (name) => {
       while (currentNode.next !== null) {
         currentNode = currentNode.next;
       }
-      currentNode.next = node;
+      currentNode.next = newNode(value);
     }
+  };
+
+  const prepend = (value) => {
+    let node = newNode(value);
+    node.next = head;
+    head = node;
   };
 
   return {
     name,
     getHead,
     append,
+    prepend,
   };
 };
 
 let myList = newLinkedList("myList");
-myList.append(newNode("test1"));
-myList.append(newNode("test2"));
-myList.append(newNode("test3"));
+myList.append("test1");
+myList.append("test2");
+myList.prepend("test3");
 
 console.log(myList.getHead());
