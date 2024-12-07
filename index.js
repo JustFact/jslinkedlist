8651eb35bd5e958455b2827aadbba65904b86b13
Jaspreet Singh <40349101+JustFact@users.noreply.github.com>
2024-12-07 15:39:24 +0530

    Complete linked list assignment

--- index.js
+++ index.js
@@ -11,14 +11,10 @@ const newLinkedList = (name) => {
     return head;
   };
   const append = (value) => {
-    //   console.log("is this working");
-
     if (head === null) {
-      // console.log("inside 1");
       head = newNode(value);
       return;
     } else {
-      // console.log("inside 2");
       let currentNode = head;
       while (currentNode.next !== null) {
         currentNode = currentNode.next;
@@ -33,17 +29,155 @@ const newLinkedList = (name) => {
     head = node;
   };
 
+  const size = () => {
+    if (head === null) {
+      return 0;
+    }
+    let currentNode = head;
+    let size = 1;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+      size = size + 1;
+    }
+    return size;
+  };
+
+  const headNode = () => {
+    return head;
+  };
+
+  const tailNode = () => {
+    if (head === null) {
+      return null;
+    }
+    let currentNode = head;
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  };
+
+  const at = (index) => {
+    let currentNode = head;
+    while (index > 0) {
+      if (currentNode.next === null) {
+        return null;
+      }
+      currentNode = currentNode.next;
+      index = index - 1;
+    }
+    return currentNode;
+  };
+
+  const pop = () => {
+    let currentNode = head;
+    head = head.next;
+    currentNode.next = null;
+    return currentNode;
+  };
+
+  const contains = (value) => {
+    let currentNode = head;
+    if (head.value === value) {
+      return true;
+    }
+
+    while (currentNode.next !== null) {
+      currentNode = currentNode.next;
+      if (currentNode.value === value) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const find = (value) => {
+    let currentNode = head;
+    let index = 0;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return index;
+      } else {
+        index = index + 1;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  };
+
+  const toString = () => {
+    let currentNode = head;
+    let string = "";
+    while (currentNode !== null) {
+      string = `${string} ( ${currentNode.value} ) ->`;
+      currentNode = currentNode.next;
+    }
+    string = string + " null";
+    return string;
+  };
+
+  const insertAt = (value, index) => {
+    let currentNode = head;
+    let pointer = 0;
+    let node = newNode(value);
+
+    if (index === 0) {
+      node.next = currentNode;
+      head = node;
+      return;
+    }
+
+    while (true) {
+      if (index - pointer === 1) {
+        node.next = currentNode.next;
+        currentNode.next = node;
+        return;
+      }
+      pointer = pointer + 1;
+      currentNode = currentNode.next;
+    }
+  };
+
+  const removeAt = (index) => {
+    if (index === 0) {
+      head = head.next;
+      return;
+    }
+    let currentNode = head;
+    let pointer = 0;
+    while (true) {
+      if (index - pointer === 1) {
+        currentNode.next = currentNode.next.next;
+        return;
+      }
+      pointer = pointer + 1;
+      currentNode = currentNode.next;
+    }
+  };
+
   return {
     name,
     getHead,
     append,
     prepend,
+    size,
+    headNode,
+    tailNode,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
   };
 };
 
-let myList = newLinkedList("myList");
-myList.append("test1");
-myList.append("test2");
-myList.prepend("test3");
-
-console.log(myList.getHead());
+let list = newLinkedList("myList");
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+console.log(list.toString());
